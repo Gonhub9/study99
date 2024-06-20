74d25470b1d8863572d051482c21bbcbc7129e18
Gonhub9 <ho_gok@naver.com>
2024-06-20 16:26:08 +0900

    [Bronze V] Title: 시험 성적, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+score = int(input().strip())
+
+if 100 >= score >= 90:
+    print('A')
+if 89 >= score >= 80:
+    print('B')
+if 79 >= score >= 70:
+    print('C')
+if 69 >= score >= 60:
+    print('D')
+if 59 >= score:
+    print('F')
